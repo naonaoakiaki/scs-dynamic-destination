e85bc8c9b56ae8d5e36be9843bf0bd1cbfe733ed
Naoaki Hashimoto <hasimoto0130gt@gmail.com>
2017-06-03 18:28:46 +0900

    init

---
+++ src/main/java/com/example/scsdynamicdestination/ScsDynamicDestinationApplication.java
@@ -0,0 +1,12 @@
+package com.example.scsdynamicdestination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScsDynamicDestinationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScsDynamicDestinationApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/scsdynamicdestination/ScsDynamicDestinationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.scsdynamicdestination;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScsDynamicDestinationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
